ead7ad552d15f357c114b366d5c9c6d15e1ce679
OMNI <omniframes79@gmail.com>
2025-08-15 08:28:04 +0100

    first commit

---
+++ ecommerce-app/frontend/src/components/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae enim, eligendi eum a modi dignissimos quod minus laudantium, vitae facere et eius aut repellat ab voluptate aliquam molestias. Quas, aspernatur.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600 cursor-pointer'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+234-813-061-9908</li>
+                <li>contact@foreveryou.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025 @forever.com - All Rights Reserved.</p>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Footer
